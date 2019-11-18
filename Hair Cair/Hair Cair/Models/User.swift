6af51298278ec63ca347ef7e7b042a484f7369a3
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2019-11-18 14:18:47 -0500

    added user type enum

---
+++ Hair Cair/Hair Cair/Models/User.swift
@@ -0,0 +1,20 @@
+//
+//  User.swift
+//  Hair Cair
+//
+//  Created by Nathan Hedgeman on 11/18/19.
+//  Copyright © 2019 Nathan Hedgeman. All rights reserved.
+//
+
+import Foundation
+
+enum TypeOfUser: String {
+    case customer = "customer"
+    case stylist = "stylist"
+    
+    static var typeOfUser: [TypeOfUser] {
+        return [.customer, .stylist]
+    }
+}
+
+
