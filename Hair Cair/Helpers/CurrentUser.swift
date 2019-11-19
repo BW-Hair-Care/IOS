7a4c44cf09f216e6ce5efc7cb3c6ba2cfacf2f60
Nate Hedgeman <47337337+hedgemanNate@users.noreply.github.com>
2019-11-18 23:32:12 -0500

    added helper folder and current user file

---
+++ Hair Cair/Helpers/CurrentUser.swift
@@ -0,0 +1,12 @@
+//
+//  CurrentUser.swift
+//  Hair Cair
+//
+//  Created by Nathan Hedgeman on 11/18/19.
+//  Copyright © 2019 Nathan Hedgeman. All rights reserved.
+//
+
+import Foundation
+
+
+var currentUser: User?
